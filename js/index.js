e1a674de4c4c0f2cf36015004525c1f65f3684c1
Peter A. Higgins <pahiggins@cenabo.com>
2018-10-08 14:48:53 +0100

    Finalise formspree integration

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 console.log('Hello');
+
+$('#contact-form').submit(function(e) {
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const message = document.getElementById('message');
+
+    if (!name.value || !email.value || !message.value) {
+        alertify.error('Name, email and message are required.');
+    } else {
+        $.ajax({
+            url: "https://formspree.io/info@walksandwhiskers.co.uk",
+            method: "POST",
+            data: $(this).serialize(),
+            dataType: "json"
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        alertify.success('Message sent!');
+    }
+});
