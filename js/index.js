fbeeb07ff9ffc97f41c74bec20b764b5a23daf6a
Peter A. Higgins <pahiggins@cenabo.com>
2018-10-09 09:42:47 +0100

    Close navigation when link is clicked

--- js/index.js
+++ js/index.js
@@ -19,3 +19,13 @@ $('#contact-form').submit(function(e) {
         alertify.success('Message sent!');
     }
 });
+
+const elements = document.getElementsByClassName('navigation__link');
+const navToggle = document.getElementById('navi-toggle');
+
+Array.prototype.slice.call(elements)
+    .forEach(function (el) {
+        el.addEventListener('click', function (e) {
+            navToggle.checked = false;
+        });
+    });
