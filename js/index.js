7887f5b290c2bc11b2f9a376853bcf7da5b106a5
Peter A. Higgins <pahiggins@cenabo.com>
2018-10-13 16:31:52 +0100

    Change logo

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-$('#contact-form').submit(function(e) {
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    const message = document.getElementById('message');
+// $('#contact-form').submit(function(e) {
+//     const name = document.getElementById('name');
+//     const email = document.getElementById('email');
+//     const message = document.getElementById('message');
 
-    if (!name.value || !email.value || !message.value) {
-        alertify.error('Name, email and message are required.');
-    } else {
-        $.ajax({
-            url: "https://formspree.io/info@walksandwhiskers.co.uk",
-            method: "POST",
-            data: $(this).serialize(),
-            dataType: "json"
-        });
-        e.preventDefault();
-        $(this).get(0).reset();
-        alertify.success('Message sent!');
-    }
-});
+//     if (!name.value || !email.value || !message.value) {
+//         alertify.error('Name, email and message are required.');
+//     } else {
+//         $.ajax({
+//             url: "https://formspree.io/p3rh5s@gmail.com",
+//             method: "POST",
+//             data: $(this).serialize(),
+//             dataType: "json"
+//         });
+//         e.preventDefault();
+//         $(this).get(0).reset();
+//         alertify.success('Message sent!');
+//     }
+// });
 
 const elements = document.getElementsByClassName('navigation__link');
 const navToggle = document.getElementById('navi-toggle');
 
 Array.prototype.slice.call(elements)
-    .forEach(function (el) {
+    .forEach(function(el) {
         el.addEventListener('click', function (e) {
             navToggle.checked = false;
         });
